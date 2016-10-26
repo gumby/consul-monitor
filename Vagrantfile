68c7e68ae4cb39c4c60f06680a7e4e34b7874dfb
gumby <pgumby@gmail.com>
2016-10-26 10:03:39 -0400

    Using bootstrap expect to create cluster

--- Vagrantfile
+++ Vagrantfile
@@ -2,25 +2,26 @@
 # vi: set ft=ruby :
 
 VAGRANTFILE_API_VERSION = "2"
+NUM_CONSUL_NODES = 3
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/xenial64"
+  config.vm.provider "virtualbox" do |v|
+    v.cpus = 1
+    v.memory = 1024
+  end
 
-  (1..3).each do |i|
-    config.vm.define "consul-node#{i}" do |node|
-      if (i == 1) 
-        type = "bootstrap"
-      else 
-        type = "server"
-      end
-      config.vm.provision "shell", path: "provisioners/install_consul.sh", args: [type, "10.100.199.20#{i}"]
-      node.vm.hostname = "consul-node#{i}"
-      node.vm.network "private_network", ip: "10.100.199.20#{i}"
+  (1..NUM_CONSUL_NODES).each do |i|
+    config.vm.define "consul-#{i}" do |node|
+      node.vm.provision "shell", 
+        path: "provisioners/install_consul.sh", 
+        args: ["vg-dc1", "10.100.199.10#{i}"]
+      node.vm.hostname = "consul-#{i}"
+      node.vm.network "private_network", ip: "10.100.199.10#{i}"
     end
   end
 
   if Vagrant.has_plugin?("vagrant-cachier")
     config.cache.scope = :box
   end
-
 end
--- provisioners/install_consul.sh
+++ provisioners/install_consul.sh
@@ -8,25 +8,26 @@ sudo apt-get autoremove
 echo Fetching consul...
 cd /tmp/
 wget --progress=bar:force https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_linux_amd64.zip -O consul.zip
+wget --progress=bar:force https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_web_ui.zip -O consul_web.zip
 
 echo Installing consul...
 unzip consul.zip
 sudo chmod +x consul
 sudo mv consul /usr/bin/consul
+sudo mkdir -p /opt/consul/web
+sudo unzip consul_web.zip -d /opt/consul/web
 
-echo Copying consul config...
+echo Modifying/copying consul config...
 sudo mkdir /etc/consul.d
 sudo chmod a+w /etc/consul.d
-sudo jq --arg addr "$2" '. + { "bind_addr": $addr }' /vagrant/conf/consul/$1/config.json > tmp.$$.json && mv tmp.$$.json /etc/consul.d/config.json
-sudo jq --arg addr "$2" '. + { "client_addr": $addr }' /etc/consul.d/config.json > tmp.$$.json && mv tmp.$$.json /etc/consul.d/config.json
+sudo jq --arg dc "$1" --arg addr "$2" '. + { "datacenter": $dc, "bind_addr": $addr, "client_addr": $addr }' /vagrant/conf/consul/config.json > tmp.$$.json
+sudo mv tmp.$$.json /etc/consul.d/config.json
 
 echo Creating consul user...
 sudo addgroup consul
 sudo useradd consul -g consul
 
-echo Create consul service...
+echo Creating consul service...
 sudo cp /vagrant/conf/service/consul.service /lib/systemd/system/
 sudo systemctl enable consul.service
 sudo systemctl start consul
-
-#consul agent -config-dir /etc/consul.d/ &> consul.log &
