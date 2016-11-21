c0bdc962aaaef7f2e0e790e896d4b1be2c4ed46b
pgombola <pgombola@pgombola-mbp.extol.com>
2016-11-21 16:28:46 -0500

    Install logstash provisioner

---
+++ vagrant/provisioners/install_logstash.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo Installing logstash...
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update && sudo apt-get install logstash
+echo Done
+
+echo Starting logstash service...
+sudo systemctl start logstash.service
+echo Done
